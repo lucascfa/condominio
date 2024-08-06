e274378913204b4de1cf86b3e1dd7f3ed1407884
lucascfa <lucas.carvalhu.ferreira@gamil.com>
2024-08-06 01:07:54 -0300

    Adicionado verificação de permissão de acordo com usuário

--- src/main/java/com/example/condominio_projeto/controller/WelcomeController.java
+++ src/main/java/com/example/condominio_projeto/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.example.condominio_projeto.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class WelcomeController {
     @GetMapping
     public String welcome(){
         return "Bem-vindo! Lucas";
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAnyRoel('USER')")
+    public String users(){
+        return "Authorized user";
+    }
+
+    @GetMapping("/managers")
+    @PreAuthorize("hasAnyRoel('MANAGER')")
+    public String managers(){
+        return "Authorized manager";
     }
 }
---
+++ src/main/java/com/example/condominio_projeto/security/WebSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.example.condominio_projeto.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig{
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/managers").hasRole("MANAGER")
+                        .requestMatchers("/users").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/", true)  // Define a URL de sucesso após login
+                        .permitAll()                    // Permite acesso à página de login para todos
+                );
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user123"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
